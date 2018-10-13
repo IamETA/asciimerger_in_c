2bca7b56c8a0ae1f1f024bf3122095f6a67e26a6
IamETA <tyleri17@student.westerdals.no>
2018-10-13 22:27:49 +0200

    Bugfix and added some statistics

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@
 CC = gcc
 
 #Flags
-CFLAGS = -g -Wall -O3 -Wextra
+CFLAGS = -g -Wall -Wextra
+DFLAGS = -O0
+RFLAGS = -O0
 
 #OBJ folder
 OBJDIR = obj
@@ -13,7 +15,7 @@ TARGET = merger
 #all: $(TARGET)
 
 $(TARGET): merger.o functions.o
-	$(CC) $(CFLAGS) -o $(TARGET) merger.o functions.o -lm
+	$(CC) $(CFLAGS) $(RFLAGS) -o $(TARGET) merger.o functions.o -lm
 
 merger.o: merger.c
 	$(CC) $(CFLAGS) -c -o merger.o merger.c
--- merger.c
+++ merger.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <sys/time.h>
 #include "include/functions.h"
 
 int main(int argc, char const *argv[])
 {
+
+    struct timeval  tv1, tv2;
+    gettimeofday(&tv1, NULL);
     if (argc < 2) {
         printf("Syntax: ./merger <inputfolder> <outputfile=[output.txt]>\n");
         exit(EXIT_FAILURE);
@@ -37,63 +41,79 @@ int main(int argc, char const *argv[])
         //Confirm positions
         printf("%s positions -> x: %i y: %i \n",currentfile,pos[i].x,pos[i].y);
     }
-    //set the size of FRAMES
+    //get the size of the FRAMES
     char * templatefile = concat(folder,files[0]);
-    int filesize = getfilesize(templatefile);
+    const int filesize = getfilesize(templatefile);
     free(templatefile);
-    //Calculate the framewidth;
-    int FRAME_WIDTH = sqrt(filesize);
+    //Calculate the framewidth based on
+    //the square root, this way we'll have an even
+    //matrix. add error handling later.
+    const int FRAME_WIDTH = sqrt(filesize);
 
+    //get the maximums of our linear matrix 
     int my = max_y(pos,filecount);
     int mx = max_x(pos,filecount);
     
-    int TOTAL_WIDTH = FRAME_WIDTH * (mx+1);
+    //calculate the maximum size of our buffer for each frame.
+    const int TOTAL_WIDTH = FRAME_WIDTH * (mx+1);
 
-    //allocate the array of pointers to file contents
+    //allocate the array of pointers to match
+    //the file sizes.
     char * outputfiledata[(mx+1)*(my+1)];
     
     //Load the files into outputfiledata, in the sequence we want
     load_in_order(mx,my,files,filecount,folder,outputfiledata,filesize,pos);
 
     //output data is now in correct order, start merge
-    int row; 
-    int page = -1; //Page multiplier
+
+    //Calculate the rows we need to loop for writing
     int totalrows = strlen(outputfiledata[0])/(FRAME_WIDTH);
+    int row; //the byte position of row
+    int page = -1; //Page multiplier
+
     FILE *fp;
     fp = fopen(outputfile,"w+");
     if(fp==NULL)
         exit(EXIT_FAILURE);
+    
     for (int y = 0; y < totalrows*(my+1) ; y++)
     {
         int pager = y % FRAME_WIDTH;
         if (pager == 0)
          page++;
-        //printf("%i:p%i\n",pager,page);
+         
+        
         row = y%FRAME_WIDTH * (TOTAL_WIDTH / (mx +1));
-        //printf("Procesing row: %i\n",row);
         int cpos = (page*(mx+1));
-        for (int m = cpos; m < cpos+(mx+1); m++)
+        for (int fileindex = cpos; fileindex < cpos+(mx+1); fileindex++)
         {
             for (int i = row; i < (row + FRAME_WIDTH); i++)
             {
-                printf("%c", outputfiledata[m][i]);
-                fputc(outputfiledata[m][i],fp);
+                printf("%c", outputfiledata[fileindex][i]);
+                fputc(outputfiledata[fileindex][i],fp);
             }
         }
         printf("\n");
         fputc('\n',fp);
     }
-    fclose(fp);
-    printf("size per page: %zu\n", strlen(outputfiledata[0]));
-    printf("Width: %i Height: %i\n",mx+1,my+1);
-    printf("totalrows per sheet:%i\n",totalrows);
+    fclose(fp);    
+    printf("Completed: %s\n", outputfile);
+
+    //Show running statistics
+    printf("size per frame: %zu\n", strlen(outputfiledata[0]));
+    printf("Frame width: %i, Frame Height: %i, width: %i, height: %i\n",mx+1,my+1,TOTAL_WIDTH,totalrows * (my+1));
+    printf("totalrows per sheet:%i\n",totalrows);    /* stuff to do! */
+    
     //Free up memory
-    printf("Freeing up memory\n");
     for (int m = 0;m<filecount;m++){
         free(outputfiledata[m]);
         free(files[m]);
     }
     free(files);
-    printf("Completed: %s\n", outputfile);
+    //end timer
+    gettimeofday(&tv2, NULL);
+    printf ("Total time spent puzzling together %s: %f seconds\n",folder,
+    (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+    (double) (tv2.tv_sec - tv1.tv_sec));
     return 0;
 }
